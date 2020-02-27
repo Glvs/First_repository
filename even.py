2aa548a765dc3f2f08a5ebb801ddd76eec98d37b
gglvs <gglvs@protonmail.com>
2020-02-27 21:09:43 +0200

    Program asks number from user and returns whether it is odd or even!

---
+++ even.py
@@ -0,0 +1,5 @@
+number = int(input("Give me a number to tell you if it is odd or even: \n"))
+if number%2 == 0: 
+	print("Number {} is even!".format(number))
+else:
+	print("Number {} is odd!".format(number))
